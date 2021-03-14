20027a33741e19c29c584d6280a997315961cf5d
digitalbrush <titus.nderitu@gmail.com>
2021-03-14 23:04:20 +0000

    Updated JS

--- js/theme.js
+++ js/theme.js
@@ -7055,25 +7055,6 @@
     }, false);
   }
 })();
-
-jQuery(document).ready(function() {
-    var $ = jQuery;
-    if ($('.set_custom_images').length > 0) {
-        if ( typeof wp !== 'undefined' && wp.media && wp.media.editor) {
-            $(document).on('click', '.set_custom_images', function(e) {
-                e.preventDefault();
-                var button = $(this);
-                var id = button.prev();
-                wp.media.editor.send.attachment = function(props, attachment) {
-                    id.val(attachment.url);
-                };
-                wp.media.editor.open(button);
-                return false;
-            });
-        }
-    }
-});
-
 jQuery(function ($) {
   $('.logo-carousel').owlCarousel({
     loop: true,
