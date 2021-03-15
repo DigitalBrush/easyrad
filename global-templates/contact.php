7f921da3fa1e34b4c6687e90939aba26cf23dc64
digitalbrush <titus.nderitu@gmail.com>
2021-03-15 04:43:37 +0000

    Contact image update

--- global-templates/contact.php
+++ global-templates/contact.php
@@ -11,12 +11,7 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 	?>
 
-<div class="section" style="background: url('<?php 
-					$attachmentID = get_option('media_selector_attachment_id');
-					$imageSizeName = "full";
-					$img = wp_get_attachment_image_src($attachmentID, $imageSizeName); 
-					echo $img[0];
-					?>)">
+<div class="section" style="background: url('<?php echo get_template_directory_uri(); ?>/img/section-besoin-d-aide-web-reverse.jpg')">
 	<div class="container-fluid contact-block" id="wrapper-static-content" tabindex="-1"  >
 		<div class="container">
 			<div class="row contact-content">
