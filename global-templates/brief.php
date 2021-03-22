094fc2f2dfb06d60a1047e6cbaa81f2495f4bd4f
digitalbrush <titus.nderitu@gmail.com>
2021-03-22 12:02:34 +0000

    Edited space in name

--- global-templates/brief.php
+++ global-templates/brief.php
@@ -16,7 +16,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="row section-brief">
 				<div class="col-sm-6 brief-content">
 					<div class="brief-text">
-						<h2 class="text-heading">Easy Rad en bref</h2>
+						<h2 class="text-heading">EasyRad en bref</h2>
 						<div class="text-body">
 							<p>EasyRad est le premier site internet à proposer la contestation en ligne des infractions relevées par radars automatiques et vidéo-verbalisation !</p>
 							<p>Vous ne savez pas comment protéger votre permis de conduire et ne pas perdre vos points ? EasyRad le fait pour vous !</p>
