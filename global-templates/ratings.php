8255b5258d7d8cb9d3fe58b0c573958fd2995a22
digitalbrush <titus.nderitu@gmail.com>
2021-03-22 11:28:06 +0000

    Ratings added to homepage hero

---
+++ global-templates/ratings.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Brief about EasyRad
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+	?>
+
+		<div class="container">
+
+			<div class="row section-ratings">
+				<div class="col-sm-12 ratings-content">
+					<div class="avis-verifies-testimonials">
+						<div style="display: inline-block; position: relative; height: 97.8359px; width: 249.85px;">
+							<div id="netreviewsWidgetNum9739widget03av" style="position: absolute; top: 0px; left: 0px; transform-origin: 0% 0% 0px; transform: scale(0.526); cursor: pointer;">
+								<div id="netreviewsWidgetNum9739widget03avBackground" style="position: relative; width: 475px; height: 186px;">
+									<img alt="4 stars" id="netreviewsWidgetNum9739widget03avImage" src="//cl.avis-verifies.com/fr/widget4/widget03.png">
+									<div id="netreviewsWidgetNum9739widget03avStars01" style="position: absolute; width: 34px; height: 34px; bottom: 13px; left: 137px;"><img alt="orange star" src="//cl.avis-verifies.com/fr/widget4/etoile_orange_full.png" style="width: 33px;height: 33px;max-width: 33px;max-height: 33px;"></div>
+									<div id="netreviewsWidgetNum9739widget03avStars02" style="position: absolute; width: 34px; height: 34px; bottom: 13px; left: 174px;"><img alt="orange star" src="//cl.avis-verifies.com/fr/widget4/etoile_orange_full.png" style="width: 33px;height: 33px;max-width: 33px;max-height: 33px;"></div>
+									<div id="netreviewsWidgetNum9739widget03avStars03" style="position: absolute; width: 34px; height: 34px; bottom: 13px; left: 209px;"><img alt="orange star" src="//cl.avis-verifies.com/fr/widget4/etoile_orange_full.png" style="width: 33px;height: 33px;max-width: 33px;max-height: 33px;"></div>
+									<div id="netreviewsWidgetNum9739widget03avStars04" style="position: absolute; width: 34px; height: 34px; bottom: 13px; left: 245px;"><img alt="orange star" src="//cl.avis-verifies.com/fr/widget4/etoile_orange_full.png" style="width: 33px;height: 33px;max-width: 33px;max-height: 33px;"></div>
+									<div id="netreviewsWidgetNum9739widget03avStars05" style="position: absolute; width: 34px; height: 34px; bottom: 13px; left: 281px;"><img alt="black star" src="//cl.avis-verifies.com/fr/widget4/etoile_orange_empty.png" style="width: 33px;height: 33px;max-width: 33px;max-height: 33px;"></div>
+									<div id="netreviewsWidgetNum9739widget03avStars06" style="position: absolute; width: 23.8px; height: 34px; bottom: 13px; left: 281px; overflow: hidden;"><img alt="orange star" src="//cl.avis-verifies.com/fr/widget4/etoile_orange_full.png" style="width: 33px;height: 33px;max-width: 33px;max-height: 33px;"></div>
+									<div id="netreviewsWidgetNum9739widget03avRateWrap" style="position: absolute; font-family: sans-serif; color: rgb(0, 0, 0); line-height: 1; font-weight: normal; width: 123px; height: 53px; font-size: 55px; bottom: 8px; right: 20px;">
+										<span id="netreviewsWidgetNum9739widget03avRate" style="font-size: inherit;">4.7</span>/<span id="netreviewsWidgetNum9739widget03avRateOver" style="font-size: inherit;">5</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<script data-minify="1" src="https://easyrad-85d9.kxcdn.com/wp-content/cache/min/1/fr/widget4/widget03.min-0638c9de9208734ce677307c83ed469f.js">
+						</script>
+					</div>
+				</div>
+			</div>
+	</div>
