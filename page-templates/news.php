481ffb6a7bac99a44816f669f4e5cdf9a5935780
digitalbrush <titus.nderitu@gmail.com>
2021-04-02 19:29:34 +0100

    Updated blog page

--- page-templates/news.php
+++ page-templates/news.php
@@ -26,27 +26,29 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="col-md-12 content-area" id="primary">
 
 				<main class="site-main" id="main" role="main" >
+					<div class="row">
 
-					<?php 
-					// the query
-					$the_query = new WP_Query( array(
-						//'category_name' => 'news',
-						'posts_per_page' => 10,
-					)); 
-					?>
+						<?php 
+						// the query
+						$the_query = new WP_Query( array(
+							//'category_name' => 'news',
+							'posts_per_page' => 10,
+						)); 
+						?>
 
-					<?php if ( $the_query->have_posts() ) : ?>
-					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-					<div class="col-md-4">
-						<div class="single-blog">
-							<div class="blog-image">
-								<?php the_post_thumbnail('full'); ?>
-							</div>
-							<div class="blog-desc">
-								<div class="blog-category"><?php the_category(); ?></div>
-								<h3 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-								<div class="description"><?php the_excerpt(); ?></div>
-								<a href="<?php the_permalink(); ?>" class="btn btn-primary">Lire la suite</a>
+						<?php if ( $the_query->have_posts() ) : ?>
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						<div class="col-md-4">
+							<div class="single-blog">
+								<div class="blog-image">
+									<?php the_post_thumbnail('full'); ?>
+								</div>
+								<div class="blog-desc">
+									<div class="blog-category"><?php the_category(); ?></div>
+									<h3 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+									<div class="description"><?php the_excerpt(); ?></div>
+									<a href="<?php the_permalink(); ?>" class="btn btn-primary">Lire la suite</a>
+								</div>
 							</div>
 						</div>
 					</div>
