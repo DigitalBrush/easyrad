c4cc70db357c3180de8e103a2f7ce051c5b2a0bd
digitalbrush <titus.nderitu@gmail.com>
2021-05-09 11:46:33 +0100

    Hover and link fix on featured section

--- global-templates/featured.php
+++ global-templates/featured.php
@@ -13,71 +13,80 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="wrapper-static-content" tabindex="-1">
 		<div class="page-section">
-			<div class="row section-benefits">
-
-				
-
+			
+			<div class="row section-benefits row-cols-md-5">
 				<div class="col-md px-0 py-md-4">
-					<div class="single-benefit">
-						<div class="benefit-desc">
-							<h4 class="title">Plus de <span>50 000</span> contestations déjà effectuées depuis 2017 et plus de 10 000 sociétés déjà clientes</h4>
+					<a href="<?php echo esc_url( home_url( '/faqs' ) ); ?>">
+						<div class="single-benefit">
+							<div class="benefit-desc">
+								<h4 class="title">Plus de <span>50 000</span> contestations déjà effectuées depuis 2017 et plus de 10 000 sociétés déjà clientes</h4>
+							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 
 				<div class="col-md px-0 py-md-4">
-					<div class="single-benefit">
-						<div class="benefit-icon">
-							<div class="avis-verifies-testimonials">
-								<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/avis-verifies.svg" />
-								<div class="review-stars">
-									<i class="material-icons md-32">star_rate</i>
-									<i class="material-icons md-32">star_rate</i>
-									<i class="material-icons md-32">star_rate</i>
-									<i class="material-icons md-32">star_rate</i>
-									<i class="material-icons md-32">star_rate</i>
+					<a href="<?php echo esc_url( home_url( '/faqs' ) ); ?>">
+						<div class="single-benefit">
+							<div class="benefit-icon">
+								<div class="avis-verifies-testimonials">
+									<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/avis-verifies.svg"  width="150"/>
+									<div class="review-stars">
+										<i class="material-icons md-32">star_rate</i>
+										<i class="material-icons md-32">star_rate</i>
+										<i class="material-icons md-32">star_rate</i>
+										<i class="material-icons md-32">star_rate</i>
+										<i class="material-icons md-32">star_rate</i>
+									</div>
 								</div>
 							</div>
+							<div class="benefit-desc">
+								<h4 class="title">4.7/5<br/>avis de nos clients</h4>
+							</div>
 						</div>
-						<div class="benefit-desc">
-							<h4 class="title">4.7/5<br/>avis de nos clients</h4>
-						</div>
-					</div>
+					</a>
 				</div>
 
 				<div class="col-md px-0 featured">
-					<div class="single-benefit">
-						
-						<div class="benefit-desc">
-							<h1 class="title price">54€<span>+ VAT</span></h1>
-							<h4 class="title">Pour une procédure habituellement facturée aux alentours de 800 euros TTC</h4>
+					<a href="<?php echo esc_url( home_url( '/faqs' ) ); ?>">
+						<div class="single-benefit">
+							
+							<div class="benefit-desc">
+								<h1 class="title price">54€<span>+ VAT</span></h1>
+								<h4 class="title">Pour une procédure habituellement facturée aux alentours de 800 euros TTC</h4>
+							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 
-				<div class="col-md px-0 py-md-4 before-last" onclick="location.href='https://maitredufour.com/cabinet-maitre-dufour-avocat-permis-de-conduire/'">
-					<div class="single-benefit">
-						<div class="benefit-icon">
-							<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/sebastien-dufour-logo.png" />
+				<div class="col-md px-0 py-md-4 before-last">
+					<a href="https://maitredufour.com/cabinet-maitre-dufour-avocat-permis-de-conduire/">
+						<div class="single-benefit">
+							<div class="benefit-icon">
+								<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/sebastien-dufour-logo.png" />
+							</div>
+							<div class="benefit-desc">
+								<h4 class="title">Un cabinet d’avocat<br/>réputé à votre service</h4>
+							</div>
 						</div>
-						<div class="benefit-desc">
-							<h4 class="title">Un cabinet d’avocat<br/>réputé à votre service</h4>
-						</div>
-					</div>
+					</a>
 				</div>
 
 				<div class="col-md px-0 py-md-4">
-					<div class="single-benefit">
-						<div class="benefit-icon">
-							<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/nnca.png" />
+					<a href="<?php echo esc_url( home_url( '/faqs' ) ); ?>">
+						<div class="single-benefit">
+							<div class="benefit-icon">
+								<img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/nnca.png" />
+							</div>
+							<div class="benefit-desc">
+								<h4 class="title">Une contestation a effectuer ou une question sur votre PV ?</h4>
+								<p class="description">Contactez le Numéro National de la Contestation Automatisée</p>
+							</div>
 						</div>
-						<div class="benefit-desc">
-							<h4 class="title">Une contestation a effectuer ou une question sur votre PV ?</h4>
-							<p class="description">Contactez le Numéro National de la Contestation Automatisée</p>
-						</div>
-					</div>
+					</a>
 				</div>
 
 			</div>
+			
 		</div>
 	</div>
